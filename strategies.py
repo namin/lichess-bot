c30a2a3db3a162633e5d534fafd2ead035203f62
Nada Amin <namin@seas.harvard.edu>
2020-10-31 07:45:44 -0400

    alpha-zero

--- strategies.py
+++ strategies.py
@@ -1,5 +1,6 @@
 import chess
 import random
+from chess_strategies import alpha_zero_strategy
 
 def random_strategy(board):
     move = random.choice(list(board.legal_moves))
@@ -8,4 +9,4 @@ def random_strategy(board):
 # Add your strategies to this dictionary.
 # Key: name
 # Value: function from chess.Board to chess.Move
-strategies = {'random':random_strategy}
+strategies = {'random':random_strategy, 'alpha-zero':alpha_zero_strategy}
